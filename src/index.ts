eec37a558d0992b89d2c7e064057957be99085c7
Rohzek <rohzek@gmail.com>
2018-04-26 11:45:00 -0500

    Trying to add CRUD for Users
    But getting a MIME type error?

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export class index{
+export class index {
 
 }
--- src/users/index.ts
+++ src/users/index.ts
@@ -1,3 +1,33 @@
-export class index{
+import {autoinject} from 'aurelia-framework';
+import {HttpClient, json} from 'aurelia-fetch-client';
+import 'fetch';
 
+@autoinject
+export class index {
+
+  constructor(private httpClient: HttpClient) {
+    httpClient.configure(config => {
+      config
+        .useStandardConfiguration()
+        .withBaseUrl('https://notesapiassignment7.azurewebsites.net/api/');
+    });
+  }
+
+  // Get user
+  async getData() {
+    this.httpClient.fetch('notes')
+      .then(response => response.json())
+      .then(data => {
+         console.log(data);
+      });
+  }
+
+  // Add user
+  postData(){}
+
+  // Update user
+  updateData(){}
+
+  // Delete user
+  deleteData(){}
 }
